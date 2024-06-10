9992a829ea373db40fd3735b5e7b40e226ba9ad4
dev-craftec <89782875+dev-craftec@users.noreply.github.com>
2024-06-10 14:04:15 +0200

    Create TFXAuthFooter.tsx

---
+++ src/components/views/auth/TFXAuthFooter.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from "react";
+
+const TFXAuthFooter = (): ReactElement => {
+    return (
+        <footer className="mx_AuthFooter" role="contentinfo"></footer>
+    );
+};
+
+export default TFXAuthFooter;
