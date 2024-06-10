6da52d6d546e4ced604cf75fe1d567ee990c3620
dev-craftec <89782875+dev-craftec@users.noreply.github.com>
2024-06-10 14:05:17 +0200

    Create TFXAuthHeaderLogo.tsx

---
+++ src/components/views/auth/TFXAuthHeaderLogo.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default class TFXAuthHeaderLogo extends React.PureComponent {
+    public render(): React.ReactElement {
+        return (
+            <aside className="mx_AuthHeaderLogo"></aside>
+        );
+    }
+}
